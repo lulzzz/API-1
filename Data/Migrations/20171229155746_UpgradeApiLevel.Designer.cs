62f698191aaae1cad5ecc0f3925b261d201c698c
Anduin <Anduin@aiursoft.com>
2017-12-29 23:58:08 +0800

    Upgrade api level

--- Data/Migrations/20171229155746_UpgradeApiLevel.Designer.cs
+++ Data/Migrations/20171229155746_UpgradeApiLevel.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Aiursoft.API.Data.Migrations
 {
     [DbContext(typeof(APIDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171229155746_UpgradeApiLevel")]
+    partial class UpgradeApiLevel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
